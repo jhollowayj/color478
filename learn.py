4be80cab7e54e6a2448a6d17ab494e091f3457ff
Matt Bray <b.matthew123@gmail.com>
2016-03-22 14:41:16 -0600

    Added mean algorithm

--- learn.py
+++ learn.py
@@ -1,6 +1,16 @@
 import sys
 from PIL import Image
 
+def get_mean(pixels):
+    r = 0
+    g = 0
+    b = 0
+    for p in pixels:
+        r += p[0]
+        g += p[1]
+        b += p[2]
+    return (r / len(pixels), g / len(pixels), b / len(pixels))
+
 if len(sys.argv) < 3:
     print "Usage: python learn.py <training_file> <test_file>"
     sys.exit(1)
@@ -27,6 +37,7 @@ pixels = []
 for p in image2.getdata():
     if p[0] in data:
         pixels.append(data[p[0]][0])
+        #pixels.append(get_mean(data[p[0]]))
     else:
         val = p[0]
         diff = 0
@@ -38,6 +49,7 @@ for p in image2.getdata():
                 val -= 1
             if val in data:
                 pixels.append(data[val][0])
+                #pixels.append(get_mean(data[val]))
                 break
 image.putdata(pixels)
 
